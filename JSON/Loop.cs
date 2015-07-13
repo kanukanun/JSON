31fedde12d5594af5889e4417eba3530ee8f2fee
kanukanun <kanukanun@gmail.com>
2015-07-13 14:56:27 +0900

    add new project

---
+++ JSON/Loop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+using Rhino;
+using Rhino.Geometry;
+using Rhino.Display;
+
+namespace JSON
+{
+    class Loop : Rhino_Processing
+    {
+        public override void Setup()
+        {
+
+        }
+        
+        public override void Draw()
+        {
+
+        }
+    }
+}
